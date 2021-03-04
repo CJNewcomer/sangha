64db1ef7fa9e0658114ed80804a8369ec3819e20
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-03-03 19:04:37 -0600

    changed confirmpassword variable

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -6,11 +6,11 @@ const SignUpForm = ({authenticated, setAuthenticated}) => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [repeatPassword, setRepeatPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const onSignUp = async (e) => {
     e.preventDefault();
-    if (password === repeatPassword) {
+    if (password === confirmPassword) {
       const user = await signUp(username, email, password);
       if (!user.errors) {
         setAuthenticated(true);
@@ -31,7 +31,7 @@ const SignUpForm = ({authenticated, setAuthenticated}) => {
   };
 
   const updateRepeatPassword = (e) => {
-    setRepeatPassword(e.target.value);
+    setConfirmPassword(e.target.value);
   };
 
   if (authenticated) {
