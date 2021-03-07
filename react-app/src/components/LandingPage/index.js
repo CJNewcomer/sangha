ca917eaa9d9f6139980d77e0965cb116356b7bfe
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-03-06 19:21:57 -0600

    nav bar styling

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import './LandingPage.css';
 
 
 const LandingPage = () => {
-    const sessionUser = useSelector((state) => state.session.user);
 
     return (
         <>
--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,78 +1,37 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import ProfileButton from './ProfileButton';
-import LogoutButton from './LogoutButton';
 
 const NavBar = () => {
-  const sessionUser = useSelector((state) => state.session.user);
-
-  let sessionLinks;
-
-  if (!!sessionUser) {
-    sessionLinks = (
-      <>
-      <li className='navbar__item'>
-        <NavLink to="/search" exact={true} activeClassName='active'>
-          Search for Community
-        </NavLink>
-      </li>
-      <li className='navbar__item'>
-        <ProfileButton user={sessionUser}/>
-      </li>
-      </>
-    );
-  }
-    // <nav>
-    //   <ul>
-    //     <li>
-    //       <NavLink to="/" exact={true} activeClassName="active">
-    //         Home
-    //       </NavLink>
-    //     </li>
-    //     <li>
-    //       <NavLink to="/users" exact={true} activeClassName="active">
-    //         Users
-    //       </NavLink>
-    //     </li>
-    //     <li>
-    //       <LogoutButton  />
-    //     </li>
-    //   </ul>
-    // </nav>
+  
     return (
-      <header className='main__container'>
-      <nav className='main__nav'>{sessionLinks}
-        <ul className='menu'>
-          <li>
-            <a className='menu__home'>
-              <i className="fas fa-compass"></i><span>Home</span>
+      <nav className='nav__container'>
+        <ul className='nav'>
+          <li className='nav__items'>
+            <a href='/home' className='menu__home'>
+              <i className="fas fa-compass"></i></a>
+          </li>
+          <li className='nav__items'>
+            {/* <input className='search' type='search' placeholder='Search'/> */}
+            <a href='/search'className='menu__search'>
+              <i className="fas fa-search"></i></a>
+          </li>
+          <li className='nav__items'>
+            <a href='/users' className='menu__profile'>
+              <i className="fas fa-user"></i>
             </a>
           </li>
-          <li>
-            <input className='search' type='search' placeholder='Search'/>
-            <a className='menu__search'>
-              <i className="fas fa-search"></i>
+          <li className='nav__items'>
+            <a href='/classes' className='menu__class'>
+              <i className="fas fa-chalkboard-teacher"></i>
             </a>
           </li>
-          <li>
-            <a className='menu__profile'>
-              <i className="fas fa-user"></i><span>My Profile</span>
-            </a>
-          </li>
-          <li>
-            <a className='menu__class'>
-              <i className="fas fa-chalkboard-teacher"></i><span>My Classes</span>
-            </a>
-          </li>
-          <li>
-            <a className='menu__message'>
-              <i className="fas fa-comments"></i><span>My Messages</span>
+          <li className='nav__items'>
+            <a href='/messages' className='menu__message'>
+              <i className="fas fa-comments"></i>
             </a>
           </li>
         </ul>
       </nav>
-    </header>  
+ 
     )       
 } 
 
