4b9ace48a09fb70e97cef785688629fcf68891af
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-03-13 22:57:15 -0600

    user profile is rendering, adding styling

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -8,14 +8,15 @@ const UserProfile = () => {
     const history = useHistory();
     const { userId } = useParams();
     
-    const user = useSelector((state) => state.users[userId]);
-    const classes = useSelector((state) => Object.values(state.classes));
+    const user = useSelector((state) => state.user[userId]);
+    const classes = useSelector((state) => Object.values(state.class));
     const sessionUser = useSelector((state) => state.session.user);
     
+
     // Grabbing all classes booked or taught
-    const myClasses = classes.filter((oneClass) => oneClass.userId === user.id);
+    const myClasses = classes.filter((oneClass) => oneClass.sessionUser.id === sessionUser.id);
     
-    if (!user) return null;
+    if (!sessionUser) return null;
 
     return (
         <>
@@ -23,19 +24,19 @@ const UserProfile = () => {
                 <div className='profile__user'>
                     <div className='profile__user-info'>
                         <div className='profile__image'>
-                            <img src={user.profile_image} alt="User Profile"/>
+                            <img src={sessionUser.profile_image} alt="User Profile"/>
                         </div>
                         <div className='profile__username'>
-                            <i>{user.username}</i>
+                            <i>{sessionUser.username}</i>
                         </div>
                         <div className='profile__name'>
-                            <i>{user.first_name} {user.last_name}</i>
+                            <i>{sessionUser.first_name} {sessionUser.last_name}</i>
                         </div>
                         <div className='profile__email'>
-                            <i>{user.email}</i>
+                            <i>{sessionUser.email}</i>
                         </div>
                         <div className='profile__bio'>
-                            <p>{user.biography}</p>
+                            <p>{sessionUser.biography}</p>
                         </div>
                         <div>
                             {!myClasses.length && <h2>No Classes Booked</h2>}
@@ -46,7 +47,7 @@ const UserProfile = () => {
             <div className='classes__main'>
                 <div classes__container>
                     {myClasses.map((myClass) => {
-                        const {class_image, name, date} = myClass;
+                        // const {class_image, name, date} = myClass;
                         return (
                             <div
                             key={myClass.userId}
@@ -55,11 +56,11 @@ const UserProfile = () => {
                                 history.push(`/users/${sessionUser.id}/myclasses/${myClass.id}`);
                             }}>
                                 <div>
-                                    <img src={class_image} alt=""/>
+                                    <img src={myClass.class_image} alt=""/>
                                     <div>
-                                        <h3>{name}</h3>
+                                        <h3>{myClass.name}</h3>
                                         <h3>{user.teacher.first_name}</h3>
-                                        <h3>{date}</h3>
+                                        <h3>{myClass.date}</h3>
                                     </div>
                                 </div>
                             </div>
