d1b731e9e218207419cf228a09d16659a8290f25
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-03-15 23:06:11 -0500

    fixing bug taught class route to profile

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -93,7 +93,16 @@ const UserProfile = ({ userProfileImage }) => {
                 </div>
                 <div className='profile__user-b'>
                     <div className='profile__bio'>
-                        <p>{sessionUser.biography}</p>
+                        {/* <p>{sessionUser.biography}</p> */}
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                            enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                            in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                            nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                            sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
                     </div>
                 </div>
             </div>
