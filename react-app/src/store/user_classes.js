3f81fba8f52be8cd67a2d8129e35036134ec599b
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-03-21 11:04:14 -0500

    adding reviews to user_classes store

--- react-app/src/store/user_classes.js
+++ react-app/src/store/user_classes.js
@@ -1,7 +1,12 @@
-const ADD = 'user_classes/ADD';
+const ADD_CLASS = 'user_classes/ADD_CLASS';
+const ADD_REVIEW = 'user_classes/ADD_REVIEW';
 
 const addClass = () => ({
-    type: ADD,
+    type: ADD_CLASS,
+})
+
+const addReview = () => ({
+    type: ADD_REVIEW,
 })
 
 // look up class_id against current user 
@@ -15,10 +20,14 @@ export const addToUserClass = (user_id, class_id) => async (dispatch) => {
     return data;
 }
 
+export const addReviewToClass = (user_id, class_id, review_id) => async (dispatch) => {
+    const res = await fetch(`/api/`)
+}
+
 
 const userClassReducer = (state={}, action) => {
     switch(action.type){
-        case ADD:
+        case ADD_CLASS:
             const newState = {...state};
             if (!newState[action.id]){
                 newState[action.id] = {id: action.id, count:1}
