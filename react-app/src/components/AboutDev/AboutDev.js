ccc0a1963cc7b35cb30d8a488ec6455dfb96e91e
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-03-25 12:20:37 -0500

    dashboard image flexbox gallery

--- react-app/src/components/AboutDev/AboutDev.js
+++ react-app/src/components/AboutDev/AboutDev.js
@@ -3,39 +3,39 @@ import React from 'react';
 const AboutDev = () => {
     return (
         <>
-            <div className='about__container'>
-                <div className='image__container'>   
-                    <img className='image__one' src='https://sangha.s3.us-east-2.amazonaws.com/court1.PNG' alt='Courtney Yoga' />
-                </div>
+            <div className='image__row'>
+              
+                    <img className='image__one' src='https://sangha.s3.us-east-2.amazonaws.com/yogacourt1.jpg' alt='Courtney Yoga' />
+             
+                    
+                    <img className='image__two' src='https://sangha.s3.us-east-2.amazonaws.com/yogacourt2.jpg' alt='Courtney Yoga'/>
+              
+                        
+                    <img className='image__three' src='https://sangha.s3.us-east-2.amazonaws.com/yogacourt3.jpg' alt='Courtney Yoga'/>
+                      
+                            
+                    <img className='image__four' src='https://sangha.s3.us-east-2.amazonaws.com/yogacourt4.jpg' alt='Courtney Yoga'/>
+                           
+                                
+                    <img className='image__five' src='https://sangha.s3.us-east-2.amazonaws.com/yogacourt5.jpg' alt='Courtney Yoga'/>
+    
                 <div>
-                    <img className='image__two' src='https://sangha.s3.us-east-2.amazonaws.com/court2.PNG' alt='Courtney Yoga'/>
-                </div>
-                <div>
-                    <img className='image__three' src='https://sangha.s3.us-east-2.amazonaws.com/court3.PNG' alt='Courtney Yoga'/>
-                </div>
-                <div>
-                    <img className='image__four' src='https://sangha.s3.us-east-2.amazonaws.com/court4.PNG' alt='Courtney Yoga'/>
-                </div>
-                <div>
-                    <img className='image__five' src='https://sangha.s3.us-east-2.amazonaws.com/court5.PNG' alt='Courtney Yoga'/>
-                </div>
-                    <div>
-                        <h5>Created by Courtney Newcomer &copy; 2021</h5>
-                    </div>
+                    <h5>Created by Courtney Newcomer &copy; 2021</h5>
                     <div className='links__container'>
                         <a className='contact__links' href='https://github.com/CJNewcomer'>
-                            <i class="fas fa-github"></i>
+                            <i class="fa fa-github"></i>
                         </a>
                         <a className='contact__links' href='https://www.linkedin.com/in/courtney-newcomer-gray-474b0923/'>
-                            <i class="fas fa-linkedin"></i>
+                            <i class="fa fa-linkedin"></i>
                         </a>
                         <a className='contact__links' href='https://angel.co/u/courtney-newcomer'>
-                            <i class="fas fa-angellist"></i>
+                            <i class="fa fa-angellist"></i>
                         </a>
                         <a className='contact__links' href=''>
-                            <i class="fas fa-globe"></i>
+                            <i class="fa fa-globe"></i>
                         </a>
                     </div>
+                </div>
             </div>
         </>
     )
