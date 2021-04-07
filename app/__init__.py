2708449252e71b7ed5b5f3a0cfe31d83c98f42a1
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-04-06 23:05:43 -0500

    bugs during heroku deployment

--- app/__init__.py
+++ app/__init__.py
@@ -83,6 +83,3 @@ def react_root(path):
     if path == 'favicon.ico':
         return app.send_static_file('favicon.ico')
     return app.send_static_file('index.html')
-
-if __name__ == '__main__':
-    socketio.run(app)
