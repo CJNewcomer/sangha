4cdf6bad713f034915e0ce721a8154f609782471
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-06-01 13:47:49 -0500

    Editing socket.io

--- react-app/src/components/Messages/index.js
+++ react-app/src/components/Messages/index.js
@@ -1,16 +1,27 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import io from "socket.io-client";
 
 import MessageTextView from './MessageTextView';
 import MessageUserView from './MessageUserView';
 
 import {getMessages} from '../../store/messages';
 import {getAllUsers} from '../../store/user';
-
 import './Message.css';
 
 const OtherUserContext = createContext();
 export const useOtherUserContext = () => useContext(OtherUserContext);
+
+// SocketIO Setup
+let socket;
+const createSocket = () => {
+    if (process.env.NODE_ENV === "production") {
+        socket = io("https://sangha-full-stack.herokuapp.com");
+    } else {    
+        socket = io();
+    }
+    return socket;
+}
 
 const Messages = () => {
     const lgdInUser = useSelector((state) => state.session.user);
@@ -38,6 +49,38 @@ const Messages = () => {
         return message.sender_id === checkId || message.receiver_id === checkId;
     });
 
+
+    // SocketIO
+    const [messages, setMessages] = useState(["Hello! Welcome!"]);
+    const [message, setMessage] = useState("");
+
+    // will call when first time app render and every time message length changes
+    const getMessages = () => {
+        socket.on('message', msg => {
+        setMessages([...messages, msg.msg.message]);
+        });
+    };
+
+    // useEffect will auto call when message length changes
+    useEffect(() => {
+        getMessages();
+    }, [messages.length]);
+
+    // if change in input field, onChange will call
+    const onChange = e => {
+        setMessage(e.target.value);
+    };
+
+    // when send button pressed, onClick will call
+    const onClick = () => {
+        if (message !== "") {
+        // when button clicked emit message to server
+        socket.emit("message", JSON.stringify({message}));
+        setMessage("");
+        } else {
+        alert("Please Add A Message");
+        }
+    };
 
     return (
         <>
